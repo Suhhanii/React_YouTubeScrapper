4c4a816aa1820b6c55624c324915ce144421b3a6
Suhhanii <suhanikaushal57@gmail.com>
2025-02-08 01:11:28 +0530

    second commit

--- src/component/MyService.js
+++ src/component/MyService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://springbootyoutubescrapper-production.up.railway.app";
 
 class MyService {
   getDetail(link) {
